9d76ff660a3c29bdbf58c4de1385e04fb9608875
D <kritantablake@gmail.com>
2022-10-22 16:35:00 +0300

    moved settings to config

---
+++ hw4/SD_libContext.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using SD_lib.Entity.Models;
+
+namespace SD_lib.DB
+{
+    public class SD_libContext : DbContext
+    {
+        private readonly IConfiguration _config;
+
+        public virtual DbSet<Client> Clients { get; set; }
+        public virtual DbSet<Card> Cards { get; set; }
+        public virtual DbSet<Account> Accounts { get; set; }
+        public virtual DbSet<AccountSession> Sessions { get; set; }
+
+
+        #region Constructor
+
+        public SD_libContext(IConfiguration config)
+        {
+            _config = config;
+            //Database.EnsureDeleted();
+            //Database.EnsureCreated();
+        }
+
+        #endregion
+
+
+        #region Creating connection
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                .UseSqlite(_config["ConnectionStrings:DefaultConnection"]);
+        }
+
+        #endregion
+
+
+        #region Creating tables
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Client>();
+            modelBuilder.Entity<Card>();
+            modelBuilder.Entity<Account>();
+            modelBuilder.Entity<AccountSession>();
+        }
+
+        #endregion
+    }
+}
+
