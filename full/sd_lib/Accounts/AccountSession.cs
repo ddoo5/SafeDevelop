6dbd10c921823a7c4ccfd74d170cc5d105221142
D <kritantablake@gmail.com>
2022-11-19 12:26:46 +0300

    account sd lib

---
+++ full/sd_lib/Accounts/AccountSession.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SD_lib.Entity.Models
+{
+    [Table("Session")]
+    public class AccountSession
+    {
+        [Column("id")]
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int SessionId { get; set; }
+
+        [Required]
+        [Column("token")]
+        [StringLength(384)]
+        public string SessionToken { get; set; }
+
+        [Column("date_created")]
+        public DateTime DateCreated { get; set; }
+
+        [Column("date_last_request")]
+        public DateTime DateLastRequest { get; set; }
+
+        [Column("date_closed")]
+        public DateTime DateClosed { get; set; }
+
+        [Column("user")]
+        public virtual Account Account { get; set; }
+    }
+}
+
