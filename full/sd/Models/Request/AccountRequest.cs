a2effa752b653c3df067bac6a3f687be46bb3db9
D <kritantablake@gmail.com>
2022-11-19 12:25:28 +0300

    requests and responses sd

---
+++ full/sd/Models/Request/AccountRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace SD.Models.Request
+{
+    public class AccountRequest
+    {
+        public string? Name { get; set; }
+
+        public string? Surname { get; set; }
+
+        public string? Email { get; set; }
+
+        public string? Password { get; set; }
+    }
+}
+
---
+++ full/sd/Models/Response/CardResponse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using SD_lib.Entity.Models;
+
+namespace SD
+{
+    public class CardResponse
+    {
+        public string CardNumber { get; set; }
+
+        public string? Name { get; set; }
+
+        public int CVV { get; set; }
+
+        public DateTime ExpirationDate { get; set; }
+
+        public virtual Client? Holder { get; set; }
+    }
+}
+
